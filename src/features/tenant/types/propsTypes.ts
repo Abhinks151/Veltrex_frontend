b73e8b5fc64e3a2ac5f3cf8f465553bdbf309f25
Abhin <abhin1511203@gmial.com>
2026-07-04 15:59:30 +0530

    feat(tenant): Implement dedicated schema and type for tenant update

--- src/features/tenant/types/propsTypes.ts
+++ src/features/tenant/types/propsTypes.ts
@@ -1,4 +1,4 @@
-import type { TenantRequest } from "./index";
+import type { TenantRequest, TenantUpdateData } from './index';
 
 export type TenantFormProps = {
   onSubmit: (data: TenantRequest) => void;
@@ -7,7 +7,7 @@ export type TenantFormProps = {
 };
 
 export type UpdateTenantFormProps = {
-  onSubmit: (data: TenantRequest) => void;
+  onSubmit: (data: TenantUpdateData) => void;
   loading: boolean;
   data: string | null;
   error: string | null;
